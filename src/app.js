2d8df73df421e832329923810e0e334367bc48a3
dev0712 <devendrakhatumariya0712@gmail.com>
2022-08-07 15:38:54 +0530

    commited all files

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'rsuite/dist/rsuite.min.css'; 
+import styles from './styles/main.scss';
+
+
+function App()
+{return(
+    <div>hello</div>
+)
+
+}
+export default App;
